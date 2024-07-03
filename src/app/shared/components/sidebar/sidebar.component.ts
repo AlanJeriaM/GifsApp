b7cd91d0819799e2447dff339f44484d90d2abfc
Alan Jeria Medina <alanjeria01@gmail.com>
2024-07-02 20:27:24 -0400

    Finalizando la seccion 7 - GifsApp

---
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -0,0 +1,25 @@
+import { Component, Injectable } from '@angular/core';
+import { GifsService } from '../../../gifs/services/gifs.service';
+
+
+
+@Component({
+  selector: 'shared-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.css'
+})
+
+
+export class SidebarComponent {
+
+  constructor(private GifsService: GifsService){}
+
+  get tags(){
+    return this.GifsService.tagsHistory;
+  }
+
+  searchTag(tag:string){
+    this.GifsService.searchTag(tag);
+  }
+
+}
